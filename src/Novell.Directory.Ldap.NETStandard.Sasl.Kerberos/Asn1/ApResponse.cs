67b921e0c744eb6f0917c9450bf002d1b6cfc4e4
Michael Stum <git@stum.de>
2018-08-12 01:48:17 -0400

    ApResponse

---
+++ src/Novell.Directory.Ldap.NETStandard.Sasl.Kerberos/Asn1/ApResponse.cs
@@ -0,0 +1,54 @@
+﻿using Novell.Directory.Ldap.Asn1;
+using System;
+using System.IO;
+
+namespace Novell.Directory.Ldap.Sasl.Asn1
+{
+    /// <summary>
+    /// AP-REP          ::= [APPLICATION 15] SEQUENCE {
+    ///         pvno            [0] INTEGER (5),
+    ///         msg-type        [1] INTEGER (15),
+    ///         enc-part        [2] EncryptedData -- EncAPRepPart
+    /// }
+    /// </summary>
+    public class ApResponse : KerberosAsn1Object
+    {
+        public static readonly Asn1Identifier Id = new Asn1Identifier(Asn1Identifier.Application, true, 15);
+
+        public int ProtocolVersionNumber { get; set; }
+        public MessageType Type { get; set; }
+        public EncryptedData EncPart { get; set; }
+
+        public ApResponse()
+            : base(Id)
+        {
+        }
+
+        public ApResponse(Asn1Tagged input, IAsn1Decoder decoder)
+            : base(Id)
+        {
+            foreach (var item in IterateThroughSequence(input, decoder, contextTagsOnly: true))
+            {
+                var itemId = item.GetIdentifier();
+                var ostring = (Asn1OctetString)item.TaggedValue;
+                switch (itemId.Tag)
+                {
+                    case 0:
+                        ProtocolVersionNumber = (int)DecodeInteger(ostring, decoder);
+                        break;
+                    case 1:
+                        Type = (MessageType)DecodeInteger(ostring, decoder);
+                        break;
+                    case 2:
+                        EncPart = new EncryptedData(item, decoder);
+                        break;
+                }
+            }
+        }
+
+        public override void Encode(IAsn1Encoder enc, Stream outRenamed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
