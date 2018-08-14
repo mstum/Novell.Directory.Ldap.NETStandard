fb6d033eb7581ed029f4960b57db4edeb7c41c2d
Michael Stum <git@stum.de>
2018-08-14 13:28:06 -0400

    Change EncryptionType to match IANA numbers

--- src/Novell.Directory.Ldap.NETStandard.Sasl.Kerberos/Asn1/EncryptionType.cs
+++ src/Novell.Directory.Ldap.NETStandard.Sasl.Kerberos/Asn1/EncryptionType.cs
@@ -1,47 +1,74 @@
 ﻿namespace Novell.Directory.Ldap.Sasl.Asn1
 {
-    /// <summary>
-    /// 
-    /// </summary>
     /// <remarks>
-    /// Taken from NTSecAPI.h
-    /// 
-    /// These encryption types are supported by the default MS KERBSUPP DLL
-    /// as crypto systems.  Values over 127 are local values, and may be changed
-    /// without notice.
+    /// These are signed values ranging from -2147483648 to 2147483647.  Positive
+    /// values should be assigned only for algorithms specified in accordance
+    /// with this specification for use with Kerberos or related protocols.
+    /// Negative values are for private use; local and experimental algorithms
+    /// should use these values.  Zero is reserved and may not be assigned.
     /// </remarks>
-    public enum EncryptionType
+    public enum EncryptionType : int
     {
-        NULL = 0,
-        DES_CBC_CRC = 1,
-        DES_CBC_MD4 = 2,
-        DES_CBC_MD5 = 3,
+        // 0: reserved
 
-        AES128_CTS_HMAC_SHA1_96 = 17,
-        AES256_CTS_HMAC_SHA1_96 = 18,
+        DES_CBC_CRC = 1, // Deprecated
 
-        // Unsupported but defined types
-        DES3_CBC_MD5 = 5,
-        DES3_CBC_SHA1 = 7,
-        DES3_CBC_SHA1_KD = 16,
+        DES_CBC_MD4 = 2, // Deprecated
 
-        // Deprecated
-        DSA_SIGN = 8,
+        DES_CBC_MD5 = 3, // Deprecated
 
-        // Pkinit encryption types
-        DSA_SHA1_CMS = 9, // also defined as KERB_ETYPE_RSA_PRIV
-        RSA_MD5_CMS = 10, // also defined as KERB_ETYPE_RSA_PUB
-        RSA_SHA1_CMS = 11, // also defined as KERB_ETYPE_RSA_PUB_MD5
-        RC2_CBC_ENV = 12, // also defined as KERB_ETYPE_RSA_PUB_SHA1
-        RSA_ENV = 13, // also defined as KERB_ETYPE_PKCS7_PUB
+        // 4: Reserved
+
+        DES3_CBC_MD5 = 5, // Deprecated
+
+        // 6: Reserved
+
+        DES3_CBC_SHA1 = 7, // Deprecated
+        
+        // 8: Unassigned
+
+        DSA_SHA1_CMS = 9,
+
+        RSA_MD5_CMS = 10,
+
+        RSA_SHA1_CMS = 11,
+
+        RC2_CBC_ENV = 12,
+
+        RSA_ENV = 13,
+
         RSA_ES_OEAP_ENV = 14,
+
         DES_EDE3_CBC_ENV = 15,
 
-        // In use types
-        DES_CBC_MD5_NT = 20,
-        RC4_HMAC_NT = 23,
-        RC4_HMAC_NT_EXP = 24,
+        DES3_CBC_SHA1_KD = 16, // Deprecated
 
+        AES128_CTS_HMAC_SHA1_96 = 17,
+
+        AES256_CTS_HMAC_SHA1_96 = 18,
+
+        AES128_CTS_HMAC_SHA256_128 = 19,
+
+        AES256_CTS_HMAC_SHA384_192 = 20,
+
+        // 21-22: Unassigned
+
+        RC4_HMAC_NT = 23, // Deprecated
+
+        RC4_HMAC_NT_EXP = 24, // Deprecated
+
+        Camellia128_CTS_CMAC = 25,
+
+        Camellia256_CTS_CMAC = 26,
+
+        // 27-64: Unassigned
+
+        SubkeyKeymaterial = 65,
+
+        // 66-2147483647: Unassigned
+
+        // Taken from NTSecAPI.h
+        // TODO: Remove them?
         RC4_MD4 = -128, // FFFFFF80
         RC4_PLAIN2 = -129,
         RC4_LM = -130,
