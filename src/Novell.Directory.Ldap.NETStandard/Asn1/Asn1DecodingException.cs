2eeb302694b2df0ea5994d7a57064cdeff1a80b4
Michael Stum <git@stum.de>
2018-08-18 14:03:45 -0400

    Asn1DecodingException: asn1Id may be null

--- src/Novell.Directory.Ldap.NETStandard/Asn1/Asn1DecodingException.cs
+++ src/Novell.Directory.Ldap.NETStandard/Asn1/Asn1DecodingException.cs
@@ -9,25 +9,25 @@ namespace Novell.Directory.Ldap.Asn1
 
         public Asn1DecodingException(Asn1Identifier asn1Id = null)
         {
-            Identifier = asn1Id.Clone();
+            Identifier = asn1Id?.Clone();
         }
 
         public Asn1DecodingException(string message, Asn1Identifier asn1Id = null)
             : base(message)
         {
-            Identifier = asn1Id.Clone();
+            Identifier = asn1Id?.Clone();
         }
 
         public Asn1DecodingException(string message, Exception innerException, Asn1Identifier asn1Id = null)
             : base(message, innerException)
         {
-            Identifier = asn1Id.Clone();
+            Identifier = asn1Id?.Clone();
         }
 
         public Asn1DecodingException(string message, int hresult, Asn1Identifier asn1Id = null)
             : base(message, hresult)
         {
-            Identifier = asn1Id.Clone();
+            Identifier = asn1Id?.Clone();
         }
     }
 }
